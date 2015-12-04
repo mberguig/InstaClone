9d5af4e6df4ce645c5d99aa0ebaf5784c6009591
mberguig <manon.berguig@gmail.com>
2015-12-04 13:56:51 +0000

    maj1

---
+++ client/app.js
@@ -0,0 +1,26 @@
+/*global angular*/
+var bcrypt = require('bcryptjs');
+var bodyParser = require('body-parser');
+var cors = require('cors');
+var express = require('express');
+var jwt = require('jwt-simple');
+var moment = require('moment');
+var mongoose = require('mongoose');
+var path = require('path');
+var request = require('request');
+
+var app = express();
+
+app.set('port', process.env.PORT || 3000);
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.listen(app.get('port'), function() {
+  console.log('Express server listening on port ' + app.get('port'));
+});
+
+angular.module('instagclone', ['ngRoute', 'ngMessages'])
+  .config(function($routeProvider) {
+  });
