f41b856318c3472a7440c6cf54d0aab25b7a46e7
mberguig <manon.berguig@gmail.com>
2015-12-04 11:49:15 +0000

    maj

--- app.js
+++ app.js
@@ -20,3 +20,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.listen(app.get('port'), function() {
   console.log('Express server listening on port ' + app.get('port'));
 });
+
+angular.module('instagclone', ['ngRoute', 'ngMessages'])
+  .config(function($routeProvider) {
+
+  });
